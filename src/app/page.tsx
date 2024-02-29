62be97ab0b4d259714370159f8bbcb4aa85de939
Luis Anaya <luis@laam.dev>
2024-02-29 23:55:20 +0100

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { MaxWidthWrapper } from '@/components/shared/max-width-wrapper'
+
+export default function Home() {
+  return (
+    <MaxWidthWrapper className='py-10'>
+      <h1>{`Next.js + Tailwind + Shadcn UI Template`}</h1>
+    </MaxWidthWrapper>
+  )
+}
